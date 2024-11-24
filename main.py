79790bd373b98606d2d783c38cb57b9db64bbc11
NRossmann <nrossmann@gmx.de>
2024-11-24 18:25:44 +0000

    Add character frequency analysis to main function

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-
 def main():
     path_to_file = "books/frankenstein.csv"
     
     with open(path_to_file) as f:
         file_contents = f.read()
-        #print(file_contents)
-        print(len(file_contents.split()))
+    
+    char_frequency = get_character_frequency(file_contents)
+    print(char_frequency)
+
+def get_character_frequency(text):
+    text = text.lower()
+    frequency = {}
+    for char in text:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency
+
 main()
